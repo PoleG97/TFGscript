91e28f4f4d5a5c5f3eeaf96558945bd673d847dc
PoleG97 <44400218+PoleG97@users.noreply.github.com>
2024-02-14 16:53:22 +0100

    Creado la instalacion de bCNC

--- bCNC.sh
+++ bCNC.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+# Actualización de paquetes
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Instalacion de paquete
+sudo apt-get install python python3-tk python3-pip -y
+
+# Instalacion de bCNC
+sudo pip install --upgrade bCNC
